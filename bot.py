51f0ec9b3fa3cdbad8bf0735da0c8973a4923884
mrfox1d <159476712+mrfox1d@users.noreply.github.com>
2026-01-20 21:22:03 +0400

    Fix typo in filename check for loading cogs

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}")
     
     for filename in os.listdir("./cogs"):
-        if filename.enswith(".py"):
+        if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
 bot.run(TOKEN)
