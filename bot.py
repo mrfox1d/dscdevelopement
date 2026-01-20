101cb566f95a30d037bf89d6b2dfbbbf8e2952c5
mrfox1d <159476712+mrfox1d@users.noreply.github.com>
2026-01-20 20:52:16 +0400

    Refactor bot.py to use environment variable for TOKEN

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
+# bot.py
 import os
 import discord
 from discord.ext import commands
 
-TOKEN = "MTM5MTM2MjgzNjEwOTcyMTYyMA.GJQMh8.cHlnr_Z4clMki4DcDctu_Sna1xJZqyPPMuGYIo"
+TOKEN = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix=".", intents=disnake.Intents.all(), help_command=None)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=".", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.playing, name="Minecraft | Сервер: AquaLand"), status=disnake.Status.dnd)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Minecraft | Сервер: AquaLand"), status=discord.Status.dnd)
     print(f"Logged in as {bot.user.name}")
-
-    bot.load_extensions("v1rago/cogs")
+    
+    for filename in os.listdir("./v1rago/cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"v1rago.cogs.{filename[:-3]}")
 
 bot.run(TOKEN)
